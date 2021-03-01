0f10d75f6101dfcd93f6aa007ed012046cb1d6c9
korsw <tkddns5158@gmail.com>
2021-03-01 23:34:19 +0900

    navi test

--- app/src/main/java/com/lsu/cafenotice/MainActivity.java
+++ app/src/main/java/com/lsu/cafenotice/MainActivity.java
@@ -82,14 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.logo_hollys
         );
 
-        for (int i = 0; i < listResId.size(); i++) {
-            Recyclerviewitem data = new Recyclerviewitem();
-            data.setName(listName.get(i));
-            data.setResId(listResId.get(i));
-            data.setWitdh(getDisplayWidth());
-
-            adapter.addItem(data);
-        }
+        for (int i = 0; i < listResId.size(); i++)
 
         adapter.notifyDataSetChanged();
     }
